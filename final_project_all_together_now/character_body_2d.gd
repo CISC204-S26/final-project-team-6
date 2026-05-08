caf3188a16b8f8aa7f6bf6f79d3f940926715a64
Flappyow <dtb@udel.edu>
2026-05-08 14:13:38 -0400

    charcter animations and movement

--- final_project_all_together_now/character_body_2d.gd
+++ final_project_all_together_now/character_body_2d.gd
@@ -1,16 +1,38 @@
 extends CharacterBody2D
 
-# Get a reference to the AnimatedSprite2D node
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	# Example: Play the "run" animation when moving
-	if velocity.length() > 0:
-		animated_sprite.play("idle")
+@export var speed := 200.0
+
+func _physics_process(delta):
+
+	# ---------------- INPUT ----------------
+	var input_vector = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("move_left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("move_up"):
+		input_vector.y -= 1
+
+	input_vector = input_vector.normalized()
+
+	# ---------------- MOVEMENT ----------------
+	velocity = input_vector * speed
+	move_and_slide()
+
+	# ---------------- ANIMATION ----------------
+	if input_vector.length() > 0:
+		if animated_sprite.animation != "run":
+			animated_sprite.play("run")
 	else:
-		animated_sprite.play("Run")
-		
-	# Flip the sprite depending on the direction of movement
+		if animated_sprite.animation != "Idle":
+			animated_sprite.play("Idle")
+
+	# ---------------- FLIP ----------------
 	if velocity.x < 0:
 		animated_sprite.flip_h = true
 	elif velocity.x > 0:
