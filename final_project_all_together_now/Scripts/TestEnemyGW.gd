62808108f9194f0056a0bf4ad82e20d74bcb407a
GeraldDW <geralddw@udel.edu>
2026-05-17 23:32:03 -0400

    Added Random Enemy to Main Scene - GW
    Added Enemy
    Added Random Direction for Enemy
    Added Timer for Enemy
    Added Test Scene for Enemy Movement
    Added Script for Enemy

---
+++ final_project_all_together_now/Scripts/TestEnemyGW.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@export var speed = 60
+@export var min_x = 0
+@export var max_x = 800
+@export var min_y = 0
+@export var max_y = 600
+
+var move_direction = Vector2.ZERO
+
+func _ready():
+	randomize()
+	pick_new_direction()
+	$Timer.start()
+
+func _physics_process(delta):
+
+	velocity = move_direction * speed
+	position += velocity * delta
+
+	# Keep enemy inside area
+	position.x = clamp(position.x, min_x, max_x)
+	position.y = clamp(position.y, min_y, max_y)
+
+func pick_new_direction():
+
+	var directions = [
+		Vector2.UP,
+		Vector2.DOWN,
+		Vector2.LEFT,
+		Vector2.RIGHT,
+		Vector2(1,1).normalized(),
+		Vector2(-1,1).normalized(),
+		Vector2(1,-1).normalized(),
+		Vector2(-1,-1).normalized()
+	]
+
+	move_direction = directions[randi() % directions.size()]
+
+func _on_timer_timeout():
+	pick_new_direction()
